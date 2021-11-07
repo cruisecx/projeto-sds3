99e8777e8df0a3db2499869ac4880ed404b2ed7b
cruisecx <holtzsecx@gmail.com>
2021-11-07 00:57:26 -0400

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import NavBar from "components/NavBar";
+import DataTable from "components/DataTable";
+
 function App() {
     return (
-        <div className="App">
-            <h1 className="text-primary">Olá, mundo!</h1>
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+
+                <h1 className="text-primary">Olá, mundo!</h1>
+
+                <DataTable />
+
+            </div>
+        </>
     );
 }
 
