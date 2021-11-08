ebd5292371ce2b28648f44f8a18f8c38ed6c6a7b
cruisecx <holtzsecx@gmail.com>
2021-11-07 22:00:45 -0400

    Last updates

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,14 +1,14 @@
 const Footer = () => {
     return (
         <div>
-            <footer className="footer mt-auto py-3 bg-dark">
+            <footer className="footer mt-auto py-3 bg-dark mt-5">
                 <div className="container">
                     <p className="text-light">App desenvolvido por <a href="https://github.com/cruisecx" target="_blank" rel="noreferrer">Wellinton Sousa</a></p>
                     <p className="text-light"><small><strong>Semana Spring React</strong><br />
                         Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
                 </div>
             </footer>
-        </div>
+        </div >
     )
 }
 
